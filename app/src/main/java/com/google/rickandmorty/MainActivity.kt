4d859f504173474fc8294fafc15e59089e3eb360
Jcquigee <gitaujaquiline@gmail.com>
2022-08-07 23:15:46 +0300

    Adding Api Service

--- app/src/main/java/com/google/rickandmorty/MainActivity.kt
+++ app/src/main/java/com/google/rickandmorty/MainActivity.kt
@@ -3,9 +3,14 @@ package com.google.rickandmorty
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+const val BASE_URL = "https://rickandmortyapi.com/api"
+
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
