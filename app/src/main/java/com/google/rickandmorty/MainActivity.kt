459d7ad18a1b95d32099b5e33f65c7bd2f3f25fd
jacqui <gitaujaquiline@gmail.com>
2023-02-27 21:51:50 +0300

    Adding network call

--- app/src/main/java/com/google/rickandmorty/MainActivity.kt
+++ app/src/main/java/com/google/rickandmorty/MainActivity.kt
@@ -2,8 +2,15 @@ package com.google.rickandmorty
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.rickandmorty.network.RickMortyApiService
+import com.squareup.moshi.Moshi
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 
-const val BASE_URL = "https://rickandmortyapi.com/api"
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,6 +18,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://rickandmortyapi.com/api/")
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+
+        val rickMortyApiService:RickMortyApiService = retrofit.create(RickMortyApiService::class.java)
+
+        rickMortyApiService.getCharacterById().enqueue(object : Callback<Any> {
+            override fun onResponse(call: Call<Any>, response: Response<Any>) {
+                Log.i("MainActivity", response.toString())
+            }
+
+            override fun onFailure(call: Call<Any>, t: Throwable) {
+                Log.i("MainActivity", t.message ?: "Null message")
+            }
+        } )
 
     }
 }
--- app/src/main/java/com/google/rickandmorty/network/RickMortyApiService.kt
+++ app/src/main/java/com/google/rickandmorty/network/RickMortyApiService.kt
@@ -1,24 +1,9 @@
 package com.google.rickandmorty.network
 
-import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.Call
 import retrofit2.http.GET
-
-private const val BASE_URL =
-    "https://rickandmortyapi.com/api"
-
-private val retrofit = Retrofit.Builder()
-    .addConverterFactory(ScalarsConverterFactory.create())
-    .baseUrl(BASE_URL)
-    .build()
-
 interface RickMortyApiService{
-    @GET("character")
-    suspend fun getCharacter(): String
+    @GET("character/2")
+    fun getCharacterById(): Call<Any>
 }
 
-object RickMortyApi {
-    val retrofitService : RickMortyApiService by lazy{
-        retrofit.create(RickMortyApiService::class.java)
-    }
-}
